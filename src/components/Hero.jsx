8a6efec30b55d1d5be85b4fa06ce89145b1969c1
Sidharth R P <sidharthrprakash@gmail.com>
2024-10-15 22:04:03 +0530

    Portfolio Website - Initial stage

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import profilePic from '/images/ProfilePic.jpg'
+
+function Hero() {
+  return (
+    <div className='flex justify-around mt-28'>
+        <div className='flex flex-col gap-5 justify-center items-left'>
+        <h1 className='text-lg'>Hey!</h1>
+        <h1 className='text-6xl'>I'm<span className='text-yellow-600'> Sidharth R Prakash </span></h1>
+        <h1 className='text-3xl'>A Fullstack Web Developer</h1>
+        <button className='mt-4 bg-yellow-600 text-black w-28 h-12 rounded-2xl text-md uppercase hover:text-white'
+         style={{ fontFamily: '"Protest Strike", sans-serif' }}>See Projects</button>
+        </div>
+        <div className='flex justify-center items-center'>
+            <img src={profilePic} className='h-72 w-72 rounded-full'></img>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+
+function HomePage() {
+  return (
+    <div>
+        <Header />
+        <Hero />
+    </div>
+  )
+}
+
+export default HomePage
